8a08532303dc6840227b92384d27cafbee41b4f2
Natan Grando <natangr@users.noreply.github.com>
2018-04-09 10:26:13 -0300

    Update shell script

--- moveToAndroidStudio.sh
+++ moveToAndroidStudio.sh
@@ -1,1 +1,1 @@
-cp -r MVVMTemplates /Applications/Android\ Studio\ 3.0.app/Contents/plugins/android/lib/templates/other/ && echo "Done!" || echo "Error!"
+cp -r MVVMTemplates /Applications/Android\ Studio.app/Contents/plugins/android/lib/templates/other/ && echo "Done!" || echo "Error!"
